9a5e3cd9ef3eefc5edb65384bcc18b31e8654dfa
Enrique de la Cruz <enrique.delacruz.r@gmail.com>
2024-05-09 23:23:13 -0600

    Primer avance del reto final

---
+++ master-spring-boot-base-project-final/src/main/java/com/LiteThinking/masterspringbootbaseprojectfinal/MasterSpringBootBaseProjectFinalApplication.java
@@ -0,0 +1,13 @@
+package com.LiteThinking.masterspringbootbaseprojectfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringBootBaseProjectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSpringBootBaseProjectFinalApplication.class, args);
+	}
+
+}
---
+++ master-spring-boot-base-project-final/src/test/java/com/LiteThinking/masterspringbootbaseprojectfinal/MasterSpringBootBaseProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LiteThinking.masterspringbootbaseprojectfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringBootBaseProjectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
